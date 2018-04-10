2f8753aa98c9d941ba36fe4dfc0cbb994a51ebff
Ray <ray@rayps.com>
2018-04-11 04:46:14 +0800

    Merge project

--- 365 Dots Extension/TodayViewController.swift
+++ 365 Dots Extension/TodayViewController.swift
@@ -10,25 +10,92 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+
+    @IBOutlet weak var labelContainer: UIView!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+
+    var yearStackView: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+
+        yearLabel.text = String(dateComponents(.year))
+        percentageLabel.text = percentageOfThisYear()
+
+        var monthViews: [UIStackView] = []
+
+        for indexOfMonth in 1...12 {
+            var dayViews: [UIView] = []
+            let daysOfMonth = (indexOfMonth % 2 == 0) ? 30 : 31
+            for indexOfDay in 1...daysOfMonth {
+                let dayView = UIView()
+                dayView.heightAnchor.constraint(equalToConstant: 4).isActive = true
+                dayView.widthAnchor.constraint(equalToConstant: 4).isActive = true
+                if (isLeapYear() && indexOfMonth == 2 && indexOfDay == 30) {
+                    dayView.backgroundColor = .clear
+                } else {
+                    let alpha: CGFloat = (indexOfMonth < dateComponents(.month) || (indexOfMonth == dateComponents(.month) && indexOfDay <= dateComponents(.day))) ? 0.25 : 1.0
+                    dayView.backgroundColor = #colorLiteral(red: 0, green: 0.9647058824, blue: 0.9176470588, alpha: 1).withAlphaComponent(alpha)
+                }
+                dayView.layer.cornerRadius = 2
+                dayView.clipsToBounds = true
+                dayViews.append(dayView)
+            }
+
+            let monthStackView = UIStackView(arrangedSubviews: dayViews)
+            monthStackView.axis = .horizontal
+            monthStackView.spacing = 4
+            monthStackView.alignment = .center
+            monthStackView.translatesAutoresizingMaskIntoConstraints = false
+
+            monthViews.append(monthStackView)
+        }
+
+        yearStackView = UIStackView(arrangedSubviews: monthViews)
+        yearStackView.axis = .vertical
+        yearStackView.spacing = 4
+        yearStackView.alignment = .center
+        yearStackView.distribution = .equalCentering
+        yearStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(yearStackView)
+        yearStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 9).isActive = true
+        yearStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    override func viewDidLayoutSubviews() {
+        labelContainer.leadingAnchor.constraint(equalTo: yearStackView.trailingAnchor, constant: 8).isActive = true
     }
-    
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
-    }
-    
+}
+
+
+
+
+
+func dateComponents(_ component: Calendar.Component) -> Int {
+    let calendar = Calendar.current
+    return calendar.component(component, from: Date())
+}
+
+func isLeapYear() -> Bool {
+    let year = dateComponents(.year)
+    return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+}
+
+func numberOfDaysInThisYear() -> Int {
+    return isLeapYear() ? 366 : 365
+}
+
+func nthOfDayInThisYear() -> Int {
+    let date = Date()
+    let calendar = Calendar.current
+    let day = calendar.ordinality(of: .day, in: .year, for: date)
+    return day!
+}
+
+func percentageOfThisYear() -> String {
+    let calculation = Float(nthOfDayInThisYear()) / Float(numberOfDaysInThisYear())
+    let result = String(calculation * 100.0).split(separator: ".").first!
+    return "\(result)%"
 }
