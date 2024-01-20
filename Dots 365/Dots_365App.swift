a84827e4f44f303c2f218161356b9c440178af03
Ray <ray@rayps.com>
2024-01-20 08:14:14 +0800

    Beginning of next version initial

---
+++ Dots 365/Dots_365App.swift
@@ -0,0 +1,17 @@
+//
+//  Dots_365App.swift
+//  Dots 365
+//
+//  Created by Ray on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Dots_365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
